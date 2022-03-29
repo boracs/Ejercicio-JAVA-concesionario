970d128a9ce3e9b405597011bc169982dffc9ce9
Mikel García Fuente <mikelgarciafuente@gmail.com>
2022-03-29 13:50:24 +0200

    prueba2

--- src/reto/Vehiculo.java
+++ src/reto/Vehiculo.java
@@ -1,5 +1,15 @@
 package reto;
 
-public class Vehiculo {
-
+public abstract class Vehiculo {
+	
+	private String registration;
+	private int numFrame;
+	private String colour;
+	private int numOfSeats;
+	private int price;
+	private int serie;
+	private String brand;
+	private String model;
+	private int year;
+	private Date fechaVenta;
 }
