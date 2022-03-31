a16952ffa1247341ca9a875f6deca25986c36e31
Mikel Garcia <mikelgarciafuente@gmail.com>
2022-03-31 23:16:11 +0200

    BD completa

--- mysql/reto_grupo_7.sql
+++ mysql/reto_grupo_7.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: reto_grupo_7
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	5.6.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,14 +21,14 @@
 
 DROP TABLE IF EXISTS `car`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `car` (
-  `registration` varchar(7) NOT NULL,
-  `numDoors` int NOT NULL,
-  `trunkCapacity` int NOT NULL,
+  `registration` varchar(10) NOT NULL,
+  `numDoors` int(11) unsigned NOT NULL,
+  `trunkCapacity` int(11) unsigned NOT NULL,
   PRIMARY KEY (`registration`),
   UNIQUE KEY `idcar_UNIQUE` (`registration`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +37,33 @@ CREATE TABLE `car` (
 
 LOCK TABLES `car` WRITE;
 /*!40000 ALTER TABLE `car` DISABLE KEYS */;
-INSERT INTO `car` VALUES ('5803BD',4,50),('6095BXN',5,40);
 /*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `series`
+--
+
+DROP TABLE IF EXISTS `series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `series` (
+  `serieNum` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `year` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`serieNum`),
+  UNIQUE KEY `serieNum_UNIQUE` (`serieNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `series`
+--
+
+LOCK TABLES `series` WRITE;
+/*!40000 ALTER TABLE `series` DISABLE KEYS */;
+/*!40000 ALTER TABLE `series` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -47,13 +72,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `truck`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `truck` (
   `registration` varchar(7) NOT NULL,
-  `load` int DEFAULT NULL,
+  `load` int(11) DEFAULT NULL,
   `merchandiseType` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`registration`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,14 +97,21 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `vehicle`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vehicle` (
-  `registration` varchar(7) NOT NULL,
-  `numFrame` varchar(45) NOT NULL,
+  `serieNum` int(11) unsigned NOT NULL,
+  `registration` varchar(10) NOT NULL,
+  `numFrame` varchar(17) NOT NULL,
   `colour` varchar(45) NOT NULL,
+  `numOfSeats` int(11) unsigned NOT NULL,
+  `price` int(11) unsigned NOT NULL,
+  `painted` tinyint(1) NOT NULL,
+  `sold` tinyint(1) NOT NULL,
+  `buyDate` datetime DEFAULT NULL,
+  `sellDate` datetime DEFAULT NULL,
   PRIMARY KEY (`registration`),
   UNIQUE KEY `id_vehicle_UNIQUE` (`registration`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +120,6 @@ CREATE TABLE `vehicle` (
 
 LOCK TABLES `vehicle` WRITE;
 /*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
-INSERT INTO `vehicle` VALUES ('1234BD','dfgdfgsdfgsdfg','blue'),('9876MGF','sd786234','white'),('B5109TY','2343489','grey');
 /*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -101,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-30 14:24:48
+-- Dump completed on 2022-03-31 23:15:16
