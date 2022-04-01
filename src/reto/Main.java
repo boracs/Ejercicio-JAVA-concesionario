d3601b867f8c42a860206fc05b557ae4b8b413e3
Iñaki <inaki.palacios.m@gmail.com>
2022-04-01 09:04:23 +0200

    Prueba commit Iñaki

--- src/reto/Main.java
+++ src/reto/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Car myCar = new Car ("Nissan", "Skyline", 1999, "3434NSR", "12345678901234567", "blue", 2, 99000, 2, 120);
+		Car inakisCar = new Car ("Nissan", "Skyline", 1999, "3434NSR", "12345678901234567", "blue", 2, 99000, 2, 120);
 		//Concessionaire.menu();
 	}
 
